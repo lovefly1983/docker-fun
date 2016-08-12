968d631f545330ca52632907c48e7b108d8bf6b2
lovefly1983 <chunjie.wangcn@gmail.com>
2016-08-12 16:55:21 +0800

    Update run-container.sh

--- pptpd/run-container.sh
+++ pptpd/run-container.sh
@@ -1,4 +1,4 @@
-CONTAINER_ID=$(docker run --name pptpd --net=host -d -t --privileged -v /root/chap-secrets:/etc/ppp/chap-secrets --restart=always -p 1723:1723 zealic/pptpd)
+CONTAINER_ID=$(docker run --name pptpd --net=host -d -t --privileged -v /root/chap-secrets:/etc/ppp/chap-secrets --restart=always -p 1723:1723 lovefly1983/pptp)
 if [ $? -ne 0 ]; then
   echo Run container failed, permission is required?
   exit 1
